c33d335984505fff16df505dd15423df7eb5ecdf
NotHakim <89632024+NotHakim@users.noreply.github.com>
2023-08-07 11:40:22 +0800

    Update backup.sh

--- Cronjob-PE/backup.sh
+++ Cronjob-PE/backup.sh
@@ -11,5 +11,10 @@ destination_dir="var/backups"
 # Run the backup using rsync
 rsync -av "$source_dir/" "$destination_dir/"
 
+#to check if the dir and file exists, if does not, it will be created
+if [ ! -e "/var/log/backup_log.txt" ]; then
+    touch "/var/log/backup_log.txt"
+fi
+
 # Log the backup action
 echo "Backup completed at: $(date)" >> /var/log/backup_log.txt
